ef1c36a6db49d89ceb702b6f209d482c55754e4f
unknown <admin@LICLABTEC-13.adserver.unisantos.br>
2025-03-06 21:35:22 -0300

    troca do formulario de add alunos para um de heroUI

--- tccWeb/src/add aluno/AddAlunoPage.tsx
+++ tccWeb/src/add aluno/AddAlunoPage.tsx
@@ -2,6 +2,9 @@ import { FormEvent, useEffect, useState } from 'react';
 import { NavLink, useNavigate } from 'react-router';
 import { useTokenStore } from '../hooks/useTokenStore';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import { Form, Input, Button } from "@heroui/react";
+import { Select, SelectItem } from "@heroui/react";
+import React from 'react';
 
 interface Avatar {
     id: number,
@@ -14,22 +17,38 @@ interface Escola {
     nome: string,
 }
 
+interface Aluno {
+
+    nome: string,
+    email: string,
+    senha: string,
+    matricula: string,
+    id_turma: number | undefined,
+    id_escola: number | undefined,
+    genero: string,
+    tipo_usuario_id: number,
+    id_avatar: number | undefined
+}
+
 export function AddAlunoPage() {
     const navigate = useNavigate();
     const { token, user } = useTokenStore();
     const [avatares, setAvatares] = useState<Avatar[]>([]);
     const [escolas, setEscolas] = useState<Escola[]>([]);
     const [contador, setContador] = useState(0);
+    const [action, setAction] = React.useState(null);
 
-    const [nome, setNome] = useState("");
-    const [id_avatar, setIdAvatar] = useState("");
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
-    const [matricula, setMatricula] = useState("");
-    const [turma, setTurma] = useState("");
-    const [escola, setEscola] = useState("");
-    const [genero, setGenero] = useState("");
-
+    const [aluno, setAluno] = useState<Aluno>({
+        nome: '',
+        email: '',
+        senha: '',
+        matricula: '',
+        id_turma: undefined,
+        id_escola: undefined,
+        genero: '',
+        id_avatar: undefined,
+        tipo_usuario_id: 2
+    })
     useEffect(() => {
         async function carregarAvatares() {
             const response = await fetch(`http://localhost:3000/avatares`, {
@@ -60,8 +79,12 @@ export function AddAlunoPage() {
         carregaEscolas();
     }, [token]);
 
+
+
     async function handleSubmit(evento: FormEvent<HTMLFormElement>) {
         evento.preventDefault();
+
+        console.log(aluno)
 
         const resposta = await fetch(`http://localhost:3000/usuarios`, {
             method: 'POST',
@@ -69,17 +92,9 @@ export function AddAlunoPage() {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${token}`
             },
-            body: JSON.stringify({
-                nome: nome,
-                email: email,
-                senha: senha,
-                id_avatar: contador + 1,
-                matricula: matricula,
-                id_turma: parseInt(turma),
-                id_escola: parseInt(escola),
-                genero: genero,
-                tipo_usuario_id: 2
-            })
+            body: JSON.stringify(
+                { ...aluno, id_avatar: (contador + 1) }
+            )
         });
 
         if (resposta.ok) {
@@ -94,82 +109,100 @@ export function AddAlunoPage() {
         const randomIndex = Math.floor(Math.random() * avatares.length);
         setContador(randomIndex);
     }
-    
+
     return (
-        <div className="size-[90vw] flex flex-col justify-center items-center gap-8">
+        <div className="size-[90vw] flex flex-col justify-start items-center gap-8">
             <h1 className="text-2xl font-bold">Adicione aqui um aluno novo</h1>
-            <form className="w-full p-5 border border-white flex flex-col justify-center items-center gap-5 mb-10" onSubmit={handleSubmit}>
-                <div className="w-full flex justify-start items-center gap-5">
-                    <label className="w-1/10 p-2">Nome</label>
-                    <input
-                        type="text"
-                        value={nome}
-                        onChange={(e) => { setNome(e.target.value) }}
-                        className="w-4/5 p-2 border-1 "
-                        required
-                    />
-                </div>
-                <div className="w-full flex justify-start items-center gap-5">
-                    <label className="w-1/10 p-2 ">Email</label>
-                    <input
-                        type="text"
-                        value={email}
-                        onChange={(e) => { setEmail(e.target.value) }}
-                        className="w-4/5 p-2 border-1 "
-                        required
-                    />
-                </div>
-                <div className="w-full flex justify-start items-center gap-5">
-                    <label className="w-1/10 p-2">Senha temporária</label>
-                    <input
-                        type="text"
-                        value={senha}
-                        onChange={(e) => { setSenha(e.target.value) }}
-                        className="w-4/5 p-2 border-1"
-                        required
-                    />
-                </div>
-                <div className="w-full flex justify-start items-center gap-5">
-                    <label className="w-1/10 p-2">Gênero</label>
-                    <select name="genero" required value={genero} onChange={(e) => { setGenero(e.target.value) }} className="w-1/5 p-2 text-base">
-                        <option className='text-white' value="">Selecione...</option>
-                        <option className='text-white' value="M">Masculino</option>
-                        <option className='text-white' value="F">Feminino</option>
-                    </select>
-                </div>
-                <div className="w-full flex justify-start items-center gap-5">
-                    <label className="w-1/10 p-2">Matrícula</label>
-                    <input
-                        type="text"
-                        maxLength={6}
-                        value={matricula}
-                        onChange={(e) => { setMatricula(e.target.value.replace(/[^0-9]/g, '')) }}
-                        className="w-1/5 p-2 border-1"
-                        required
-                    />
-                </div>
-                <div className="w-full flex justify-start items-center gap-5">
-                    <label className="w-1/10 p-2">Turma</label>
-                    <select name="turma" required value={turma} onChange={(e) => { setTurma(e.target.value) }} className="w-1/5 p-2 text-base">
-                        <option value="">Selecione...</option>
-                        <option className='text-white' value="1">1 ano</option>
-                        <option className='text-white' value="2">2 ano</option>
-                        <option className='text-white' value="3">3 ano</option>
-                    </select>
-                </div>
-                <div className="w-full flex justify-start items-center gap-5">
-                    <label className="w-1/10 p-2">Escola</label>
-                    <select name="escola" required value={escola} onChange={(e) => { setEscola(e.target.value) }} className="w-1/5 p-2 text-base">
-                        <option value="">Selecione...</option>
-                        {
-                            escolas.map((escola, index) => {
-                                return (
-                                    <option className='text-white' key={index} value={index + 1}>{escola.nome}</option>
-                                );
-                            })
-                        }
-                    </select>
-                </div>
+            <Form
+                className="w-[80%] flex flex-col justify-center  gap-4 border-2 p-10 border-white"
+                onReset={() => setAction("reset")}
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    let data = Object.fromEntries(new FormData(e.currentTarget));
+                    handleSubmit(e)
+                }}
+            >
+                <Input
+                    isRequired
+                    errorMessage="Coloque um nome valido"
+                    label="Nome"
+                    labelPlacement="outside"
+                    onChange={(e) => { setAluno({ ...aluno, nome: e.target.value }) }}
+                    value={aluno.nome}
+                    name="nome"
+                    placeholder="Nome..."
+                    type="text"
+                    classNames={{
+                        label: '!text-white'
+                    }}
+                />
+
+                <Input
+                    isRequired
+                    errorMessage="Insira um email válido"
+                    label="Email"
+                    labelPlacement="outside"
+                    onChange={(e) => { setAluno({ ...aluno, email: e.target.value }) }}
+                    value={aluno.email}
+                    name="email"
+                    placeholder="Email..."
+                    type="email"
+                    classNames={{
+                        label: '!text-white'
+                    }}
+                />
+
+                <Input
+                    isRequired
+                    errorMessage="Insira uma senha válida"
+                    label="Senha"
+                    labelPlacement="outside"
+                    onChange={(e) => { setAluno({ ...aluno, senha: e.target.value }) }}
+                    value={aluno.senha}
+                    name="senha"
+                    placeholder="Senha temporária..."
+                    type="text"
+                    classNames={{
+                        label: '!text-white'
+                    }}
+
+                />
+
+                <Select onChange={(e) => { setAluno({ ...aluno, genero: e.target.value }) }} value={aluno.genero} className="max-w-xs " label="Selecione o gênero">
+                    <SelectItem key={'M'} className='text-black' >Masculino</SelectItem>
+                    <SelectItem key={'F'} className='text-black' >Feminino</SelectItem>
+                </Select>
+
+                <Input
+                    isRequired
+                    className="max-w-xs "
+                    errorMessage="Insira uma matrícula válida de 6 digitos"
+                    label="Matricula"
+                    onChange={(e) => { setAluno({ ...aluno, matricula: e.target.value }) }}
+                    value={aluno.matricula}
+                    labelPlacement="outside"
+                    name="matricula"
+                    placeholder="Matricula..."
+                    type="text"
+                    maxLength={6}
+                    minLength={6}
+                    classNames={{
+                        label: '!text-white'
+                    }}
+                />
+
+                <Select onChange={(e) => { setAluno({ ...aluno, id_turma: parseInt(e.target.value) }) }} value={aluno.turma} className="max-w-xs " label="Selecione o gênero">
+                    <SelectItem key={1} className='text-black' >1° ano</SelectItem>
+                    <SelectItem key={2} className='text-black' >2° ano</SelectItem>
+                    <SelectItem key={3} className='text-black' >3° ano</SelectItem>
+                </Select>
+
+                <Select onChange={(e) => { setAluno({ ...aluno, id_escola: parseInt(e.target.value) }) }} value={aluno.escola} className="max-w-xs " label="Selecione a escola">
+                    {escolas.map((escola) => (
+                        <SelectItem className='text-black' key={escola.id}>{escola.nome}</SelectItem>
+                    ))}
+                </Select>
+
                 <div className="w-full flex justify-start items-center gap-5">
                     <label className="w-1/10 p-2">Avatar</label>
                     <div className="flex w-1/5 justify-center items-center flex-col gap-5">
@@ -188,23 +221,32 @@ export function AddAlunoPage() {
                             </button>
                         </div>
                     </div>
-                </div>
-    
-                <h2 className="text-xl font-bold">Previsualização</h2>
-                <div className="w-1/4 bg-yellow-400 flex justify-around items-center text-black text-lg p-2 rounded cursor-pointer transition-transform duration-300 ease-in-out">
-                    {avatares[contador] ? (
-                        <img className="w-1/5 rounded-full" src={avatares[contador].caminho} alt="" />
-                    ) : (
-                        <img src="/src/assets/userDefault.png" alt="" />
-                    )}
-    
-                    <p>{nome ? nome : 'Nome aluno'}</p>
-                    <p>Lvl 1</p>
-                </div>
-                <button className="bg-cyan-400 text-black p-2 rounded cursor-pointer">Mandar</button>
-            </form>
 
-           
+
+                    <div className="w-[40%] bg-yellow-400 flex justify-around items-center text-black text-lg p-2 rounded cursor-pointer transition-transform duration-300 ease-in-out">
+                        {avatares[contador] ? (
+                            <img className="w-1/5 rounded-full" src={avatares[contador].caminho} alt="" />
+                        ) : (
+                            <img src="/src/assets/userDefault.png" alt="" />
+                        )}
+
+                        <p>{aluno.nome ? aluno.nome : 'Nome aluno'}</p>
+                        <p>Lvl 1</p>
+                    </div>
+
+                </div>
+
+
+                <div className="flex gap-2">
+                    <Button color="primary" type="submit">
+                        Submit
+                    </Button>
+                    <Button type="reset" variant="flat">
+                        Reset
+                    </Button>
+                </div>
+
+            </Form>
         </div>
     );
 }
